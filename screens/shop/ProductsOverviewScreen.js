37f9a5c78e945921de25d06fcc52a77c78ffe10f
Antonio Sucre <antonio.sucre@indikatore.com>
2021-11-24 05:47:20 -0300

    styling-product-items

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FlatList, View, Text, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
+import ProductItem from "../../components/shop/ProductItem";
 
 const ProductOverviewScreen = props => {
     const products = useSelector(state => state.products.availableProducts);
@@ -8,7 +9,14 @@ const ProductOverviewScreen = props => {
     return (
         <FlatList
             data={products}
-            renderItem={itemData => <Text>{itemData.item.title}</Text>}
+            renderItem={itemData =>
+                <ProductItem
+                    image={itemData.item.imageUrl}
+                    title={itemData.item.title}
+                    price={itemData.item.price}
+                    onViewDetail={() => { }}
+                    onAddToCart={() => { }}
+                />}
         />
 
     )
