dc265312cc2b45231b092f35699fb55f05cf8753
Antonio Sucre <antonio.sucre@indikatore.com>
2021-11-24 04:34:21 -0300

    configuring-reducer

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { FlatList, View, Text, StyleSheet } from "react-native";
+
+const ProductOverviewScreen = props => {
+    return (
+        <FlatList />
+
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default ProductOverviewScreen;
