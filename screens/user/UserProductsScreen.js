bfa91722b6a00ff26bb243ca4ca573b6b4c4cc16
Antonio Sucre <antonio.sucre@indikatore.com>
2021-11-25 15:22:46 -0300

    delete-products

--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
-import { useSelector } from 'react-redux';
+import { View, Text, StyleSheet, FlatList, Button, } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import ProductItem from '../../components/shop/ProductItem';
 import HeaderButton from "../../components/UI/HeaderButton";
+import Colors from '../../constants/Colors';
+import * as productsActions from '../../store/actions/products';
 
 const UserProductScreen = props => {
-    const userProducts = useSelector(state => state.products.userProducts)
+    const userProducts = useSelector(state => state.products.userProducts);
+    const dispatch = useDispatch();
+
     return (
         <FlatList
             data={userProducts}
@@ -16,9 +20,15 @@ const UserProductScreen = props => {
                     image={itemData.item.imageUrl}
                     title={itemData.item.title}
                     price={itemData.item.price}
-                    onViewDetail={() => { }}
-                    onAddToCart={() => { }}
-                />
+                    onSelect={() => { }}
+                >
+                    <Button color={Colors.primary} title="Edit" onPress={() => {
+
+                    }} />
+                    <Button color={Colors.primary} title="Delete" onPress={() => {
+                        dispatch(productsActions.deleteProduct(itemData.item.id));
+                    }} />
+                </ProductItem>
             }
         />
     )
