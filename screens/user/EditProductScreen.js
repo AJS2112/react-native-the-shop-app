e2bbb3bac234b786f700e62912354aa87595afe0
Antonio Sucre <antonio.sucre@indikatore.com>
2021-11-27 06:14:21 -0300

    dispatching-actions-edit-product

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { View, ScrollView, Text, TextInput, StyleSheet } from "react-native";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import HeaderButton from "../../components/UI/HeaderButton";
+import * as productsActions from '../../store/actions/products';
 
 const EditProductScreen = props => {
 
@@ -12,30 +13,45 @@ const EditProductScreen = props => {
     const editedProduct = useSelector(state =>
         state.products.userProducts.find(prod => prod.id === prodId)
     );
+    const dispatch = useDispatch();
 
     const [title, setTitle] = useState(editedProduct ? editedProduct.title : '');
     const [imageUrl, setImageUrl] = useState(editedProduct ? editedProduct.imageUrl : '');
     const [price, setPrice] = useState(editedProduct ? editedProduct.price : '');
     const [description, setDescription] = useState(editedProduct ? editedProduct.description : '');
+
+    const submitHandler = useCallback(() => {
+        if (editedProduct) {
+            dispatch(productsActions.updateProduct(prodId, title, description, imageUrl));
+        } else {
+            dispatch(productsActions.createProduct(title, description, imageUrl, +price));
+
+        }
+    }, [dispatch, prodId, title, description, imageUrl, price]);
+
+    useEffect(() => {
+        props.navigation.setParams({ submit: submitHandler });
+    }, [submitHandler])
+
     return (
         <ScrollView>
             <View style={styles.form}>
                 <View style={styles.formControl}>
                     <Text style={styles.label}>Title</Text>
-                    <TextInput style={styles.input} value={title} onChange={text => setTitle(text)} />
+                    <TextInput style={styles.input} value={title} onChangeText={text => setTitle(text)} />
                 </View>
                 <View style={styles.formControl}>
                     <Text style={styles.label}>image URL</Text>
-                    <TextInput style={styles.input} value={imageUrl} onChange={text => setImageUrl(text)} />
+                    <TextInput style={styles.input} value={imageUrl} onChangeText={text => setImageUrl(text)} />
                 </View>
                 {editedProduct ? null :
                     <View style={styles.formControl}>
                         <Text style={styles.label}>Price</Text>
-                        <TextInput style={styles.input} value={price} onChange={text => setPrice(text)} />
+                        <TextInput style={styles.input} value={price} onChangeText={text => setPrice(text)} />
                     </View>}
                 <View style={styles.formControl}>
                     <Text style={styles.label}>Description</Text>
-                    <TextInput style={styles.input} value={description} onChange={text => setDescription(text)} />
+                    <TextInput style={styles.input} value={description} onChangeText={text => setDescription(text)} />
                 </View>
             </View>
         </ScrollView>
@@ -43,7 +59,7 @@ const EditProductScreen = props => {
 };
 
 EditProductScreen.navigationOptions = navData => {
-    console.log(navData.navigation.getParam('productId'))
+    const submitFn = navData.navigation.getParam('submit');
     return {
         headerTitle: navData.navigation.getParam('productId') ? 'Edit Product' : 'Add Product',
         headerRight: () => (
@@ -51,9 +67,7 @@ EditProductScreen.navigationOptions = navData => {
                 <Item
                     title='Save'
                     iconName={'md-checkmark'}
-                    onPress={() => {
-
-                    }}
+                    onPress={submitFn}
                 />
             </HeaderButtons>),
     }
