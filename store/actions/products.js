fc6f6660d93f0eabe3bec06eeb724109f88e53f7
Antonio Sucre <antonio.sucre@indikatore.com>
2021-11-30 11:34:37 -0300

    display-loading-spinner-handling-errors

--- store/actions/products.js
+++ store/actions/products.js
@@ -8,26 +8,36 @@ export const SET_PRODUCTS = 'SET_PRODUCTS';
 
 export const fetchProducts = () => {
     return async dispatch => {
-        const response = await fetch(
-            'https://rn-complete-guide-bc624-default-rtdb.firebaseio.com/products.json',
-        );
+        try {
 
-        const resData = await response.json();
-        const loadedProducts = [];
+            const response = await fetch(
+                'https://rn-complete-guide-bc624-default-rtdb.firebaseio.com/products.jon',
+            );
 
-        for (const key in resData) {
-            loadedProducts.push(
-                new Product(
-                    key,
-                    'u1',
-                    resData[key].title,
-                    resData[key].imageUrl,
-                    resData[key].description,
-                    resData[key].price
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const resData = await response.json();
+            const loadedProducts = [];
+
+            for (const key in resData) {
+                loadedProducts.push(
+                    new Product(
+                        key,
+                        'u1',
+                        resData[key].title,
+                        resData[key].imageUrl,
+                        resData[key].description,
+                        resData[key].price
+                    )
                 )
-            )
+            }
+            dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+        } catch (err) {
+            throw err;
         }
-        dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+
     }
 }
 
